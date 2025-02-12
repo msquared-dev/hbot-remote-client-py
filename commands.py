56beddcc5bbb8c5947c7759fdaf433e13a07c172
Asman Malikov <asmanmalikov@gmail.com>
2025-02-12 16:02:54 +0200

    add trades

---
+++ commands.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import asyncio
+from hbotrc import BotListener, BotCommands
+import pprint
+import json
+async def run_commands(client):
+    # resp = client.start()
+    # resp = client.status()
+
+    await asyncio.sleep(1)
+    resp = client.full_report(days=10)
+    pprint.pprint(json.loads(resp.report))
+    await asyncio.sleep(1)
+
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) < 2:
+        print('Give instance_id argument')
+    _id = sys.argv[1]
+    client = BotCommands(
+        host='localhost',
+        port=1883,
+        username='',
+        password='',
+        bot_id=_id,
+    )
+    asyncio.new_event_loop().run_until_complete(run_commands(client))
